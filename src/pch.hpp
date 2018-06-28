6004344a263af0ca44089603817a771d98b79e6e
Tomas Pruzina <tomas.pruzina@gmail.com>
2018-06-29 00:37:38 +0200

    Update list of precompiled headers to speed up incremental builds 	modified:   pch.hpp

--- src/pch.hpp
+++ src/pch.hpp
@@ -1,11 +1,38 @@
 #ifndef PCH_HPP
 #define PCH_HPP
 
-#include <QtWidgets>
-#include <QtDebug>
-#include <QSystemTrayIcon>
+#include <algorithm>
+#include <chrono>
+#include <cstring>
+#include <dirent.h>
+#include <fstream>
+#include <inttypes.h>
+#include <iostream>
+#include <mutex>
+#include <QCryptographicHash>
 #include <QDialog>
+#include <QObject>
+#include <QPushButton>
+#include <QSettings>
+#include <QSharedMemory>
+#include <QStandardPaths>
+#include <QSystemSemaphore>
+#include <QSystemTrayIcon>
+#include <QTabWidget>
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
+#include <QtDebug>
+#include <QtWidgets>
+#include <QWidget>
+#include <shared_mutex>
+#include <sstream>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <thread>
+#include <time.h>
+#include <unordered_set>
+#include <vector>
 
 #endif // PCH_HPP
