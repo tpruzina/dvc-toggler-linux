7b7a969174d3c375d974925992072b2702a173fd
Tomas Pruzina <tomas.pruzina@gmail.com>
2018-07-11 01:13:34 +0200

    move nvidia screen check back up in main

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,14 @@ int main(int argc, char *argv[])
 	Q_INIT_RESOURCE(dvc_toggler_linux);
 	QApplication app(argc, argv);
 
+	// check whether DVC enabled nvidia screen is available
+	if(!NVIDIA::isScreenAvailable())
+	{
+		QMessageBox::critical(0, QObject::tr("DVC toggler"),
+				      QObject::tr("I couldn't detect any DVC enabled Nvidia screen."));
+		return 1;
+	}
+
 	// check whether we are already running
 	RunGuard guard("dvc_toggler_linux");
 	if (!guard.tryToRun())
@@ -19,15 +27,6 @@ int main(int argc, char *argv[])
 		bus.sendSignal((char*)"show()");
 		return 0;
 	}
-	
-	// check whether DVC enabled nvidia screen is available
-	if(!NVIDIA::isScreenAvailable())
-	{
-		QMessageBox::critical(0, QObject::tr("DVC toggler"),
-				      QObject::tr("I couldn't detect any DVC enabled Nvidia screen."));
-		return 1;
-	}
-
 
 	QApplication::setQuitOnLastWindowClosed(false);
 
