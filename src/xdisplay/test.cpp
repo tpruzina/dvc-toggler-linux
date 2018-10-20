4e242d704be0e3ba1f79e4e1c3c093bf37d5126c
Tomas Pruzina <tomas.pruzina@gmail.com>
2018-10-20 22:06:00 +0200

    [test] XDisplay check return value

--- src/xdisplay/test.cpp
+++ src/xdisplay/test.cpp
@@ -5,7 +5,18 @@
 int main()
 {
         auto pid = XDisplay::queryFocusedWindowPID();
-        std::cout << "query_focused_window_pid(): " <<
+        if(pid < 0)
+        {
+                std::cerr << "query_focused_window_pid() returned "
+                        << pid 
+                        << " (this should only happen if "
+                        << "there is no focused window or X isn't running)\n";
+                return -1;
+        }
+        else
+        {
+                std::cout << "query_focused_window_pid(): " <<
                 pid << '\n';
+        }
         return 0;
 }
